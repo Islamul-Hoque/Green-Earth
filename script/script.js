82e4d67b02603f53142f9e855a9070633ad109a6
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-09-08 01:31:04 +0600

    Integrate dynamic cart addition for selected trees

--- script/script.js
+++ script/script.js
@@ -24,16 +24,40 @@ const displayTreesCard =(TreesCards)=>{  //Display Trees Card
             TreesCardDiv.innerHTML = `
                 <div class="bg-white rounded-[1rem] shadow-sm p-4 flex flex-col h-full">
                     <img class="rounded-[0.8rem] w-full h-56 md:h-52 object-cover" src="${TreesCard.image}" alt="">
-                    <h3 class="font-semibold  text-[1.1rem] mt-3">Mango Tree</h3>
+                    <h3 class="font-semibold  text-[1.1rem] mt-3">${TreesCard.name}</h3>
                     <p class="mt-2 text-[0.8rem] flex-grow">${TreesCard.description}</p>
                     <div class="flex justify-between items-center my-3 ">
                         <div class="text-[#15803d] bg-[#dcfce7] text-[0.8rem] px-4 py-2 rounded-[1rem]">${TreesCard.category}</div>
                         <div class="font-semibold">৳<span>${TreesCard.price}</span></div>
                     </div>
-                    <button class="btn btn-success shadow-none text-white w-full rounded-[0.8rem]">Plant a Tree</button>
+                    <button class="addToCard btn btn-success shadow-none text-white w-full rounded-[0.8rem]">Add to Cart</button>
                 </div>
             `
             treesCardContainer.appendChild(TreesCardDiv)
+        
+
+            // Add to Cart handle 
+            const addBtn = TreesCardDiv.querySelector(".addToCard");
+            addBtn.addEventListener('click', () => {
+
+            const priceCardContainer = getId('price-Card-Container');
+            const priceDiv = document.createElement('div');
+            priceDiv.innerHTML = `
+                <div class="bg-[#dcfce7] p-4 md:p-2 rounded-[0.6rem] my-2 flex justify-between items-center">
+                    <div>
+                        <h3 class="font-semibold mb-1">${TreesCard.name}</h3>
+                        <div class="text-[0.8rem]">৳<span>${TreesCard.price}</span> x 1</div>
+                    </div>
+                    <div><i class="fa-solid fa-xmark"></i></div>
+                </div>
+            `;
+            priceCardContainer.appendChild(priceDiv);
+            
+
+        
+
+            });
+
         });
 }
 
