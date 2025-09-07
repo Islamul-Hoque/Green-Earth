4ee88782311b42c5543e1028c7ffeb160f4d10bb
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-09-07 20:06:13 +0600

    Load categories from API and render dynamically

--- script/script.js
+++ script/script.js
@@ -3,9 +3,22 @@ function getId(id){
     return document.getElementById(id);
 }
 
-const loadCategories =()=> {
+const loadCategories =()=> {  //categories btn name load from Api
     fetch('https://openapi.programming-hero.com/api/categories')
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => displayCategories(data.categories));
 }
 
+
+const displayCategories =(Categories)=>{ // display categories btn name 
+    const categoriesContainer = getId('categories-Container');
+    categoriesContainer.innerHTML = ''; 
+        Categories.forEach(category => {
+            const categoriesDiv = document.createElement('div');
+            categoriesDiv.innerHTML = ` <button onclick="loadTreesCard('${category.id}')" class="btn btn-outline border-none btn-success w-full Black my-1">${category.category_name}</button> `
+            categoriesContainer.appendChild(categoriesDiv)
+            // console.log(category);
+        });
+}
+
+loadCategories();
