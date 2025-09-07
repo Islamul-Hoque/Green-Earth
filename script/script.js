08cc4977d41ccfe7d6ba8e2c49ac7860e4c40a05
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-09-07 20:41:28 +0600

    Display category-specific trees in detailed 3-column cards

--- script/script.js
+++ script/script.js
@@ -9,8 +9,35 @@ const loadCategories =()=> {  //categories btn name load from Api
     .then(data => displayCategories(data.categories));
 }
 
+const loadTreesCard =(id)=> {  //load Trees Card from Api
+    
+    fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    .then(res => res.json())
+    .then(data => displayTreesCard(data.plants));
+}
 
-const displayCategories =(Categories)=>{ // display categories btn name 
+const displayTreesCard =(TreesCards)=>{  //Display Trees Card
+    const treesCardContainer = getId('Trees-Card-Container');
+    treesCardContainer.innerHTML = '';
+        TreesCards.forEach(TreesCard => {
+            const TreesCardDiv = document.createElement('div')
+            TreesCardDiv.innerHTML = `
+                <div class="bg-white rounded-[1rem] shadow-sm p-4 flex flex-col h-full">
+                    <img class="rounded-[0.8rem] w-full h-56 md:h-52 object-cover" src="${TreesCard.image}" alt="">
+                    <h3 class="font-semibold  text-[1.1rem] mt-3">Mango Tree</h3>
+                    <p class="mt-2 text-[0.8rem] flex-grow">${TreesCard.description}</p>
+                    <div class="flex justify-between items-center my-3 ">
+                        <div class="text-[#15803d] bg-[#dcfce7] text-[0.8rem] px-4 py-2 rounded-[1rem]">${TreesCard.category}</div>
+                        <div class="font-semibold">৳<span>${TreesCard.price}</span></div>
+                    </div>
+                    <button class="btn btn-success shadow-none text-white w-full rounded-[0.8rem]">Plant a Tree</button>
+                </div>
+            `
+            treesCardContainer.appendChild(TreesCardDiv)
+        });
+}
+
+const displayCategories =(Categories)=>{ // Display categories btn name 
     const categoriesContainer = getId('categories-Container');
     categoriesContainer.innerHTML = ''; 
         Categories.forEach(category => {
