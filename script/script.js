47a987034c1c62a80e792f3e1c18bef4bfc8ca36
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-09-09 01:14:36 +0600

    Augment tree card section with a loading spinner during API requests

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-// Reusable Function
+// Reusable get ID Function
 function getId(id){
     return document.getElementById(id);
 }
 
-// Add to Cart handle function
+// Reusable Add to Cart function
 const addToCartBtn = (tree) => {
     const priceCardContainer = getId('price-Card-Container');
 
@@ -52,7 +52,19 @@ const displayTreeModal = (modal)=>{
     treesModal.showModal();
 }
 
-//categories single btn name load from Api
+// Reusable Spinner
+const manageSpinner =(status)=>{
+    if(status == true){
+        getId('spinner').classList.remove('hidden');
+        getId('Trees-Card-Container').classList.add('hidden');
+    }
+    else{
+        getId('spinner').classList.add('hidden');
+        getId('Trees-Card-Container').classList.remove('hidden');
+    }
+}
+
+//Categories single btn name load from Api
 const loadCategories =()=> { 
     fetch('https://openapi.programming-hero.com/api/categories')
     .then(res => res.json())
@@ -68,6 +80,8 @@ const loadAllTrees = ()=>{
 
 // Display All Trees
 const displayAllTrees = (AllTrees)=>{
+    manageSpinner(true);
+
     const treesCardContainer = getId('Trees-Card-Container');
     treesCardContainer.innerHTML = '';
 
@@ -79,7 +93,7 @@ const displayAllTrees = (AllTrees)=>{
                     <h3 class="font-semibold  text-[1.1rem] mt-3 tree-Name cursor-pointer">${AllTree.name}</h3>
                     <p class="mt-2 text-[0.8rem] flex-grow">${AllTree.description}</p>
                     <div class="flex justify-between items-center my-3 ">
-                        <div class="text-[#15803d] bg-[#dcfce7] text-[0.8rem] px-4 py-2 rounded-[1rem]">${AllTree.category}</div>
+                        <div class="text-[#15803d] bg-[#dcfce7] text-[0.8rem] px-4 py-2 Geist-Font rounded-[1rem]">${AllTree.category}</div>
                         <div class="font-semibold">৳<span>${AllTree.price}</span></div>
                     </div>
                     <button class="addToCard btn btn-success shadow-none text-white w-full rounded-[0.8rem]">Add to Cart</button>
@@ -94,23 +108,48 @@ const displayAllTrees = (AllTrees)=>{
         TreesCardDiv.querySelector(".tree-Name")
         .addEventListener('click', () => displayTreeModal(AllTree));
     })
+    manageSpinner(false);
 };
 
 // Event listener for "All Trees" button
 getId('all-Trees-Btn').addEventListener('click', () => {
+    // Remove active class
+    clickRemoveActive();
+
+    // Add active to All Trees btn
+    getId('all-Trees-Btn').classList.add('active');
+
     loadAllTrees();
 });
-loadAllTrees();
+// loadAllTrees();
+
+// Remove active class
+const clickRemoveActive =()=>{
+    const clickRemove = document.querySelectorAll(".click-Remove");
+    clickRemove.forEach(btn => btn.classList.remove('active'));
+}
 
 //load Trees Card from Api
 const loadTreesCard =(id)=> {  
     fetch(`https://openapi.programming-hero.com/api/category/${id}`)
     .then(res => res.json())
-    .then(data => displayTreesCard(data.plants));
+    .then(data => {
+        //Remove all active class
+        clickRemoveActive(); 
+
+        //Add active class
+        const clickBtn = getId(`Trees-id-${id}`);
+        clickBtn.classList.add('active');
+
+        displayTreesCard(data.plants);
+    });
 }
+
 
 //Display Trees Card
 const displayTreesCard =(TreesCards)=>{  
+    manageSpinner(true);
+
     const treesCardContainer = getId('Trees-Card-Container');
     treesCardContainer.innerHTML = '';
 
@@ -122,7 +161,7 @@ const displayTreesCard =(TreesCards)=>{
                     <h3 class="font-semibold text-[1.1rem] mt-3 tree-Name cursor-pointer">${TreesCard.name}</h3>
                     <p class="mt-2 text-[0.8rem] flex-grow">${TreesCard.description}</p>
                     <div class="flex justify-between items-center my-3 ">
-                        <div class="text-[#15803d] bg-[#dcfce7] text-[0.8rem] px-4 py-2 rounded-[1rem]">${TreesCard.category}</div>
+                        <div class="text-[#15803d] bg-[#dcfce7] text-[0.8rem] Geist-Font px-4 py-2 rounded-[1rem]">${TreesCard.category}</div>
                         <div class="font-semibold">৳<span>${TreesCard.price}</span></div>
                     </div>
                     <button class="addToCard btn btn-success shadow-none text-white w-full rounded-[0.8rem]">Add to Cart</button>
@@ -137,6 +176,8 @@ const displayTreesCard =(TreesCards)=>{
             TreesCardDiv.querySelector(".tree-Name")
             .addEventListener('click', () => displayTreeModal(TreesCard));
         });
+
+    manageSpinner(false);
 }
 
 // Display categories btn name 
@@ -145,7 +186,7 @@ const displayCategories =(Categories)=>{
     categoriesContainer.innerHTML = ''; 
         Categories.forEach(category => {
             const categoriesDiv = document.createElement('div');
-            categoriesDiv.innerHTML = ` <button onclick="loadTreesCard('${category.id}')" class="btn btn-outline text-left justify-start border-none btn-success w-full Black my-1">${category.category_name}</button> `
+            categoriesDiv.innerHTML = ` <button id="Trees-id-${category.id}" onclick="loadTreesCard('${category.id}')" class="btn btn-outline text-left justify-start border-none btn-success w-full Black my-1 click-Remove">${category.category_name}</button> `
             categoriesContainer.appendChild(categoriesDiv)
         });
 }
